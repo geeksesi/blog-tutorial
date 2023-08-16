0008708a0859f351f9d5b4976aefdf9fad5ac28d
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-08-16 22:30:19 +0330

    feat: php dotenv

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ use App\Services\RoutingService;
 
 include __DIR__ . "/vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $routingService = new RoutingService();
 
 $routingService->execute();
