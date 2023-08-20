e5b642c44099a33feabdad25a0ba89287dbac63b
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-08-20 09:22:44 +0330

    feat: some function on PostModel

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -12,6 +12,8 @@ final class PostModel extends BaseModel
         'title' => PDO::PARAM_STR,
         'slug' => PDO::PARAM_STR,
         'body' => PDO::PARAM_STR,
+        'created_at' => PDO::PARAM_STR,
+        'thumbnail' => PDO::PARAM_STR,
     ];
 
     public static function getAll()
@@ -25,5 +27,29 @@ final class PostModel extends BaseModel
             return false;
         }
         return $query->fetchAll(PDO::FETCH_CLASS, static::class);
+    }
+
+    public function createdAtHumanFormat(): string
+    {
+        $date = \DateTime::createFromFormat('Y-m-d H:i:s', $this->created_at);
+        return $date->format('dS F, Y');
+    }
+
+    public function summery(): string
+    {
+        if (strlen($input) <= $length)
+            return $input;
+
+        $parts = explode(" ", $input);
+
+        while (strlen(implode(" ", $parts)) > $length)
+            array_pop($parts);
+
+        return sprintf("%s...", $summery);
+    }
+
+    public function url(): string
+    {
+        return sprintf("/post/%s", $this->slug);
     }
 }
